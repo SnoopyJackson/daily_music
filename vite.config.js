7a761bd7fdffe77f227c612459006fc1a7e55fed
SnoopyJackson <jonathangabaret@gmail.com>
2026-06-05 14:21:59 +0200

    Add GitHub Pages deployment, fix base path, add .gitignore

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  base: '/daily_music/',
+  plugins: [react()],
   server: {
     port: 3000,
-    https: true,
   },
 })
